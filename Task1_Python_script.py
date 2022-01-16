99068f7fb51df49064fcaf42b69d6950bf0daace
Alfonso Diaz-Infante <alfonsodiazinfante@hotmail.com>
2022-01-16 10:09:02 -0500

    Sending comment for review

--- Task1_Python_script.py
+++ Task1_Python_script.py
@@ -1,4 +1,5 @@
 # Code completed by Alfonso Diaz Infante
+# Comment for review
 # Python script to scrape an article given the url of the article and store the extracted text in a file
 # Url: https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
 
